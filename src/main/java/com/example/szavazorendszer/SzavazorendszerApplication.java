3f84092d4c06f4830948bf23e4575f2567b91230
Pelyko <silye.peter98@gmail.com>
2023-12-21 12:27:17 +0100

    Initial commit

---
+++ src/main/java/com/example/szavazorendszer/SzavazorendszerApplication.java
@@ -0,0 +1,13 @@
+package com.example.szavazorendszer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SzavazorendszerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SzavazorendszerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/szavazorendszer/SzavazorendszerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.szavazorendszer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SzavazorendszerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
